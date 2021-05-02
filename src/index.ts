870f22f81836c23de6b4b730a40ccca4cec9b57b
Vitor Carvalho <vitorblz@gmail.com>
2021-05-02 12:33:15 -0300

    ajuste prettier

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
+import dotenv from 'dotenv';
+import ServerConfig from './config/ServerConfig';
+import app from './server';
+
+dotenv.config();
+
+app.listen(ServerConfig.PORT, () => console.log(`Servidor rodando na porta ${ServerConfig.PORT}`));
+
 console.log('teste');
