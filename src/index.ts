98050d390790eca005779c4e0a049abb1fa9ad8f
Vitor Carvalho <vitorblz@gmail.com>
2021-05-03 20:35:26 -0300

    feat: config ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,4 @@
-import dotenv from 'dotenv';
 import ServerConfig from './config/ServerConfig';
 import app from './server';
 
-dotenv.config();
-
 app.listen(ServerConfig.PORT, () => console.log(`Servidor rodando na porta ${ServerConfig.PORT}`));
-
-console.log('teste');
